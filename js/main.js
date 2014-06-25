d82c6284373e7be9bb65e1fe8d0349dceec116f5
Tony Tambe <tambe257@gmail.com>
2014-06-25 13:47:55 -0400

    Completed part 3 of the tutorial

--- js/main.js
+++ js/main.js
@@ -11,24 +11,35 @@ app.controller('MyController', function($scope) {
 	updateClock();
 });
 
-app.controller('PlayerController', ['$scope', function($scope) {
-	$scope.playing = false;
-	$scope.audio = document.createElement('audio');
-	$scope.audio.src = 'Lazaretto.mp3';
-	$scope.play = function() {
-		$scope.audio.play();
-		$scope.playing = true;
-	};
-	$scope.stop = function() {
-		$scope.audio.pause();
-		$scope.playing = false;
-	};
-	$scope.audio.addEventListener('ended', function() {
-		$scope.$apply(function() {
-			$scope.stop()
-		});
-	});	
-}]);
+var apiKey = 'MDA4NjY5ODI4MDEzMjM0NzcwNDY2ZGFmNA001',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
+app.controller('PlayerController', function($scope, $http) {
+	//$scope.playing = false;
+	//$scope.audio = document.createElement('audio');
+	//$scope.audio.src = 'Lazaretto.mp3';
+	//$scope.play = function() {
+	//	$scope.audio.play();
+	//	$scope.playing = true;
+	//};
+	//$scope.stop = function() {
+	//	$scope.audio.pause();
+	//	$scope.playing = false;
+	//};
+	//$scope.audio.addEventListener('ended', function() {
+	//	$scope.$apply(function() {
+	//		$scope.stop()
+	//	});
+	//});
+
+	$http({
+		method: 'JSONP',
+		url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'}).success(function(data, status){
+			$scope.programs = data.list.story;
+		}).error(function(data, status){
+
+		});	
+});
 
 app.controller('RelatedController', ['$scope', function($scope) {
 
